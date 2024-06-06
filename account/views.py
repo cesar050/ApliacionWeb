aa1ce490c2b521fc517a236eacd651b67e7c0e1a
davosJar <alexis.jara@unl.edu.ec>
2024-06-06 17:56:00 -0500

    apirest

--- account/views.py
+++ account/views.py
@@ -1,11 +1,12 @@
-from django.contrib.auth import authenticate, login
-from django.http import HttpResponse
+from django.contrib.auth import login
+from django.http import JsonResponse
 from django.shortcuts import render, redirect
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
 from django.views import View
 from .forms import LoginForm
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate
 
 @method_decorator(csrf_exempt, name='dispatch')
 class LoginView(View):
@@ -21,7 +22,20 @@ class LoginView(View):
         else:
             return render(request, 'account/login.html', {'form': form})
 
-
 @login_required
 def dashboard(request):
     return render(request, 'dashboard.html')
+
+@csrf_exempt
+def api_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return JsonResponse({'message': 'Login successful'})
+        else:
+            return JsonResponse({'message': 'Invalid credentials'}, status=401)
+    else:
+        return JsonResponse({'message': 'Only POST requests are allowed'}, status=405)
